feda83766d0bee75eb353402fd8e456103abeeb9
Zlobin Vladimir <vladimir.zlobin@intel.com>
2020-10-31 12:24:53 +0300

    Update env

--- a2c.py
+++ a2c.py
@@ -1,9 +1,9 @@
 import collections
 import datetime
 import itertools
-import random
 import time
 
+import cv2
 import gym
 import gym.wrappers
 import gym.envs.atari
@@ -12,27 +12,38 @@ import torch
 from torch.utils import tensorboard
 
 
-class RenderWrapper(gym.Wrapper):
-    def step(self, action):
-        ret = super().step(action)
-        super().render()
-        return ret
+class ImshowWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.time_point = float('-inf')
+
+    def observation(self, observation):
+        now = time.perf_counter()
+        if now - self.time_point >= 0.5:
+            cv2.imshow('', observation[:, :, ::-1])
+            cv2.waitKey(1)
+            self.time_point = now
+        return observation
 
 
 class CropGrayScaleResizeWrapper(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
-        self.shape = (94, 72)
+        self.shape = (94, 74)
         self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=self.shape, dtype=np.float32)
 
     def observation(self, observation):
         # observation.shape is (210, 160, 3)
-        return observation[5:-17:2, 8:-8:2, :].mean(2, dtype=np.float32) / 255
+        return observation[5:-17:2, 7:-6:2, :].mean(2, dtype=np.float32) / 255
 
 
-def breakout():
-    return gym.wrappers.FrameStack(CropGrayScaleResizeWrapper(gym.wrappers.TimeLimit(gym.envs.atari.AtariEnv('breakout', obs_type='image',
-        frameskip=4, repeat_action_probability=0.25), max_episode_steps=60000)), 4)
+def create_env(show=False):
+    name = 'breakout'
+    if show:
+        return gym.wrappers.FrameStack(gym.wrappers.AtariPreprocessing(ImshowWrapper(gym.wrappers.TimeLimit(gym.envs.atari.AtariEnv(
+            name, obs_type='image', frameskip=4), max_episode_steps=2_000)), frame_skip=1, terminal_on_life_loss=True, scale_obs=True), 3)
+    return gym.wrappers.FrameStack(gym.wrappers.AtariPreprocessing(gym.wrappers.TimeLimit(gym.envs.atari.AtariEnv(
+        name, obs_type='image', frameskip=4), max_episode_steps=2_000), frame_skip=1, terminal_on_life_loss=True, scale_obs=True), 3)
 
 
 def conv(in_features, out_features, kernel_size=3, stride=1, padding=1):
@@ -51,7 +62,7 @@ class ActorCritic(torch.nn.Module):
             conv(32, 64, kernel_size=4, stride=2, padding=2),
             conv(64, 64),
             torch.nn.Flatten(),
-            torch.nn.Linear(8320, 512),
+            torch.nn.Linear(9216, 512),
             torch.nn.ReLU())
         self.n_alternatives = n_alternatives
         if self.n_alternatives == 1:
@@ -76,7 +87,7 @@ class ActorCritic(torch.nn.Module):
 
 def train_step(mem, detached_next_values, actor_critic):
     gamma = 0.99
-    gamma_lambda = gamma * 0.92
+    gamma_lambda = gamma * 0.95
     actor_losses = []
     critic_losses = []
     gae = 0.0
@@ -92,22 +103,16 @@ def train_step(mem, detached_next_values, actor_critic):
 
     actor_critic.optimizer.zero_grad()
     (torch.stack(actor_losses).mean() + 0.25 * critic_loss).backward()
-    torch.nn.utils.clip_grad_norm_(actor_critic.parameters(), 0.5)
+    torch.nn.utils.clip_grad_norm_(actor_critic.parameters(), 0.4)
     actor_critic.optimizer.step()
     return critic_loss.item()
 
 
 def main():
-    Entry = collections.namedtuple('Entry', ['distrs', 'values', 'actions', 'rewards', 'inverted_dones'])
-    SEED = 0xACE
-    random.seed(SEED)
-    np.random.seed(SEED)
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed(SEED)
+    # TODO random search that cuts bad params in the beginning
+    Entry = collections.namedtuple('Entry', ('distrs', 'values', 'actions', 'rewards', 'inverted_dones'))
     torch.set_default_tensor_type(torch.cuda.FloatTensor)
-
-    envs = gym.vector.async_vector_env.AsyncVectorEnv((breakout,) * 16)  # 5 + (lambda: RenderWrapper(breakout()),)
-    envs.seed(SEED)
+    envs = gym.vector.async_vector_env.AsyncVectorEnv((lambda: create_env(show=True),) + (create_env,) * 38)
     try:
         n_actions = envs.action_space[0].n
     except AttributeError:
@@ -116,12 +121,13 @@ def main():
 
     current_scores = np.zeros((envs.num_envs,), dtype=np.float32)
     last_scores = []
+    max_mean_score = float('-inf')
     critic_losses = []
+    mem = []
     states = torch.as_tensor(envs.reset())
     summary_writer = tensorboard.SummaryWriter(datetime.datetime.now().strftime('logs/%d-%m-%Y %H-%M'))
     t0 = time.perf_counter()
     for step_id in itertools.count():
-        mem = []
         for _ in range(5):
             distrs, values = actor_critic(states)
             actions = distrs.sample()
@@ -139,19 +145,23 @@ def main():
             if dones.any():
                 last_scores.extend(current_scores[dones])
                 current_scores[dones] = 0.0
-                if len(last_scores) > 999:
+                if len(last_scores) > 249:
                     scores = np.mean(last_scores)
                     val_loss = np.mean(critic_losses)
                     summary_writer.add_scalar('Scalars/Score', scores, step_id)
                     summary_writer.add_scalar('Scalars/Value loss', val_loss, step_id)
-                    print(step_id, scores, val_loss, time.perf_counter() - t0)
-                    last_scores = []
-                    critic_losses = []
+                    dur = (time.perf_counter() - t0) / 60.0
+                    if max_mean_score < scores:
+                        max_mean_score = scores
+                    print(step_id, scores, val_loss, dur, dur / 60.0, max_mean_score)
+                    last_scores *= 0
+                    critic_losses *= 0
 
         with torch.no_grad():
             detached_next_values = actor_critic(next_states, False)
         critic_loss = train_step(mem, detached_next_values, actor_critic)
         critic_losses.append(critic_loss)
+        mem *= 0
 
 
 if __name__ == '__main__':

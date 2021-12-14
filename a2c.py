8b2e09b0c18c070d0402513b4fbe16044241b169
Wovchena <vladimir.zlobin@intel.com>
2021-12-14 20:49:48 +0300

    Scale critic_loss by 5.0

--- a2c.py
+++ a2c.py
@@ -22,7 +22,7 @@ class ImshowWrapper(gym.ObservationWrapper):
         if self.count >= self.limit:
             self.count = 0
             cv2.imshow('', observation)  # TODO show it while the net is trained. This will also allow to show multiple observations at a time and draw graphs of and critic loss, actions distribution on an image and state embedding
-            key = cv2.waitKey(1)
+            key = cv2.pollKey()
             if key != -1:
                 key = chr(key)
                 if '0' <= key <= '9':
@@ -32,7 +32,35 @@ class ImshowWrapper(gym.ObservationWrapper):
         return observation
 
 
-class CropResizeGrayScaleWrapper(gym.ObservationWrapper):
+class MetaRenderWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.count = 0
+        self.limit = 9 * 100
+
+    def step(self, action):
+        observation, reward, done, diagnostic_info = super().step(action)
+        if self.count >= self.limit:
+            self.count = 0
+            cv2.imshow('', diagnostic_info['rgb'][:, :, ::-1])
+            key = cv2.pollKey()
+            if key != -1:
+                key = chr(key)
+                if '0' <= key <= '9':
+                    self.limit = int(key) * 100
+        else:
+            self.count += 1
+        return observation, reward, done, diagnostic_info
+
+
+class RenderWrapper(gym.Wrapper):
+    def step(self, action):
+        ret = super().step(action)
+        super().render()
+        return ret
+
+
+class CropScaleWrapper(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
         self.shape = (94, 147)
@@ -40,7 +68,17 @@ class CropResizeGrayScaleWrapper(gym.ObservationWrapper):
 
     def observation(self, observation):
         # observation.shape is (210, 160)
-        return observation[5:-17:2, 7:-6].astype(np.float32) / 255.0
+        return observation[5:-17:2, 7:-6].astype(np.float32) / 255.0  # TODO without scale
+
+
+class ScaleWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        old = env.observation_space
+        self.observation_space = gym.spaces.Box(low=0.0, high=0.0, shape=old.shape, dtype=np.float32)
+
+    def observation(self, observation):
+        return observation.astype(np.float32) / 255.0
 
 
 class StopScoreOnLifeLossWrapepr(gym.Wrapper):
@@ -62,13 +100,54 @@ class StopScoreOnLifeLossWrapepr(gym.Wrapper):
                 return observation, reward, False, {None: True}
 
 
-def imgenv(show=False):
-    envargs = {'game': 'space_invaders', 'mode': None, 'difficulty': None, 'obs_type': 'grayscale', 'frameskip': 5, 'repeat_action_probability': 0.25, 'full_action_space': True, 'render_mode': None}
+class CountTimeLimit(gym.wrappers.TimeLimit):
+    def __init__(self, env, max_episode_steps):
+        super().__init__(env, max_episode_steps)
+        self._elapsed_steps = 0
+        self.longest_session = 0
+
+    def reset(self, **kwargs):
+        if self.longest_session < self._elapsed_steps:
+            self.longest_session = self._elapsed_steps
+        return super().reset(**kwargs)
+
+    def close(self):
+        print(f'{self.longest_session = } ')
+        super().close()
+
+
+class InvertDonesWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def step(self, action):
+        observation, reward, done, diagnostic_info = super().step(action)
+        if done:
+            return observation, reward, True, {None: False}
+        return observation, reward, False, {None: True}
+
+
+def imenv(show=False):
+    envargs = {'game': 'breakout', 'mode': None, 'difficulty': None, 'obs_type': 'grayscale', 'frameskip': 5, 'repeat_action_probability': 0.25, 'full_action_space': True, 'render_mode': None}
     if show:
-        return gym.wrappers.FrameStack(ImshowWrapper(StopScoreOnLifeLossWrapepr(CropResizeGrayScaleWrapper(gym.wrappers.TimeLimit(gym.envs.atari.AtariEnv(
+        return gym.wrappers.FrameStack(ImshowWrapper(StopScoreOnLifeLossWrapepr(CropScaleWrapper(CountTimeLimit(gym.envs.atari.AtariEnv(
             **envargs), max_episode_steps=50_000)))), 1)
-    return gym.wrappers.FrameStack(StopScoreOnLifeLossWrapepr(CropResizeGrayScaleWrapper(gym.wrappers.TimeLimit(gym.envs.atari.AtariEnv(
+    return gym.wrappers.FrameStack(StopScoreOnLifeLossWrapepr(CropScaleWrapper(CountTimeLimit(gym.envs.atari.AtariEnv(
         **envargs), max_episode_steps=50_000))), 1)
+
+
+def ramenv(show=False):
+    envargs = {'game': 'breakout', 'mode': None, 'difficulty': None, 'obs_type': 'ram', 'frameskip': 5, 'repeat_action_probability': 0.25, 'full_action_space': True}
+    if show:
+        return StopScoreOnLifeLossWrapepr(MetaRenderWrapper(ScaleWrapper(CountTimeLimit(gym.envs.atari.AtariEnv(render_mode='rgb_array', **envargs), max_episode_steps=50_000))))
+    return StopScoreOnLifeLossWrapepr(ScaleWrapper(CountTimeLimit(gym.envs.atari.AtariEnv(**envargs), max_episode_steps=50_000)))
+
+
+def classicenv(show=False):
+    envname = 'CartPole-v1'
+    if show:
+        return RenderWrapper(InvertDonesWrapper(gym.make(envname)))
+    return InvertDonesWrapper(gym.make(envname))
 
 
 def conv(in_features, out_features, kernel_size=3, stride=1, padding=1):
@@ -85,12 +164,20 @@ class ActorCritic(torch.nn.Module):
         super().__init__()  # TODO MLP backbone
         self.extractor = torch.nn.Sequential(
             conv(state_dim, 32, kernel_size=8, stride=4, padding=4),
-            conv(32, 64, kernel_size=4, stride=2, padding=2),
+            conv(32, 64, kernel_size=4, stride=2, padding=2),  # TODO 264 channles trains slower but reaches highter res
             conv(64, 64),
             torch.nn.Flatten(),
             torch.nn.Linear(15808, 512),
-            torch.nn.LeakyReLU())
-        self.lstm = torch.nn.LSTMCell(512+n_alternatives, 513)  # TODO GRU
+            torch.nn.LeakyReLU()) if state_dim == 1 else torch.nn.Sequential(
+                torch.nn.Linear(state_dim, 128),  # TODO 256
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(128, 128),
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(128, 128),
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(128, 512),  # TODO dropout
+                torch.nn.LeakyReLU())
+        self.lstm = torch.nn.LSTMCell(512+n_alternatives, 513)  # TODO GRU  # TODO 513->512
         self.actor = torch.nn.Linear(513, n_alternatives)
         self.critic = torch.nn.Linear(513, 1)
         self.n_alternatives = n_alternatives
@@ -143,9 +230,9 @@ def train_step(mem, detached_next_values, actor_critic):
 
     actor_loss = -20.0 * torch.stack(actor_losses).mean()
     entropy_loss = torch.stack(entropy_losses).mean()
-    critic_loss = torch.stack(critic_losses).mean()
+    critic_loss = 5.0 * torch.stack(critic_losses).mean()
 
-    actor_critic.optimizer.zero_grad()
+    actor_critic.optimizer.zero_grad(set_to_none=True)
     (actor_loss - entropy_loss + critic_loss).backward()
     torch.nn.utils.clip_grad_norm_(actor_critic.parameters(), 0.4)
     actor_critic.optimizer.step()
@@ -168,13 +255,15 @@ def randplay(envs):
                 print(f'{baseline = }')
                 return torch.as_tensor(next_observations), baseline
 
+
 def main():
     # TODO random search that cuts bad params in the beginning
-    # TODO report longest game to set more accurate max_episode_steps
     # TODO assume game continues when max_episode_steps is hit
+    torch.autograd.set_detect_anomaly(False)
+    torch.backends.cudnn.benchmark = True
     torch.set_default_tensor_type(torch.cuda.FloatTensor)
     # TODO try wrappers from stable-baselines3
-    with gym.vector.async_vector_env.AsyncVectorEnv((lambda: imgenv(show=True),) + (imgenv,) * 39) as envs:
+    with gym.vector.async_vector_env.AsyncVectorEnv((lambda: imenv(show=True),) + (imenv,) * 39) as envs:
         states, max_mean_score = randplay(envs)
         try:
             n_actions = envs.action_space[0].n
@@ -213,7 +302,7 @@ def main():
                     prev_h = prev_h[0] * inverted_dones, prev_h[1] * inverted_dones  # Stop grad for completed envs
                     last_scores.extend(current_scores[dones])
                     current_scores[dones] = 0.0
-                    if len(last_scores) > 249:
+                    if len(last_scores) > 249:  # TODO Rerpot every 5 mins
                         scores = np.mean(last_scores)
                         mean_losses = np.mean(weighted_losses, axis=0)
                         mins = round((time.perf_counter() - t0) / 60.0)
@@ -232,12 +321,12 @@ def main():
                         summary_writer.add_scalar('Losses/Actor', mean_losses[0], step_idk)
                         summary_writer.add_scalar('Losses/Entropy', mean_losses[1], step_idk)
                         summary_writer.add_scalar('Losses/Critic', mean_losses[2], step_idk)  # TODO report distribution over actions
-                        print(f'{color}{step_idk:5,}k {hours:2}:{mins:2} {scores:7,.1f} {mean_losses}')
+                        print(f'{color}{step_idk:5,}k {hours:2}:{mins:2} {scores:7,.1f} {mean_losses}')  # TODO tqdm
 
             with torch.no_grad():
                 detached_next_values = actor_critic(next_states, one_hot, prev_h, value_only=True)
             weighted_loss = train_step(mem, detached_next_values, actor_critic)
-            prev_h = prev_h[0].detach(), prev_h[1].detach()  # TODO don't stop grad
+            prev_h = prev_h[0].detach(), prev_h[1].detach()
             weighted_losses.append(weighted_loss)
             mem *= 0
 
